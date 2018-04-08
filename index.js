0c436512569b3739016acb7bce5720bd06073f2e
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-08 00:17:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,40 @@
+function Point(x,y) {
+  this.x = x;
+  this.y = y;
+}
+
+Point.prototype.toString = function() => {
+  return `(${this.x},${this.y})`;
+}
+
+function Side() {
+
+}
+
+function Shape() {
+
+}
+
+function Circle() {
+
+}
+
+function Polygon() {
+
+}
+
+function Quadrilateral() {
+
+}
+
+function Rectangle() {
+
+}
+
+function Square() {
+
+}
+
+function Triangle() {
+
+}
